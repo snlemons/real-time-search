4809a125234cd533d8bf7a2af5054993b6d60179
snlemons <sofia.lemons@cs.unh.edu>
2016-04-26 00:23:25 -0400

    Added javadoc for SlidingTilePuzzle.kt

--- src/main/kotlin/edu/unh/cs/ai/realtimesearch/environment/slidingtilepuzzle/SlidingTilePuzzle.kt
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/environment/slidingtilepuzzle/SlidingTilePuzzle.kt
@@ -4,6 +4,14 @@ import edu.unh.cs.ai.realtimesearch.environment.Domain
 import edu.unh.cs.ai.realtimesearch.environment.SuccessorBundle
 import java.lang.Math.abs
 
+/**
+ * The sliding tile puzzle is a toy domain in which numbered tiles and a blank
+ * space are shifted (swapping the blank with any of the four adjacent numbered
+ * tiles), with the goal of placing the numbered tiles in numerical order. 
+ * Assumed to be square.
+ *
+ * @param size is both the width and height of the puzzle (giving n^2 - 1 tiles and a blank.)
+ */
 class SlidingTilePuzzle(val size: Int, val actionDuration: Long) : Domain<SlidingTilePuzzle4State> {
     val goalState: SlidingTilePuzzle4State by lazy {
         val state = SlidingTilePuzzle4State(0, 0, 0.0)
@@ -16,6 +24,9 @@ class SlidingTilePuzzle(val size: Int, val actionDuration: Long) : Domain<Slidin
         state
     }
 
+    /**
+     * Part of the Domain interface.
+     */
     override fun successors(state: SlidingTilePuzzle4State): List<SuccessorBundle<SlidingTilePuzzle4State>> {
         val successorBundles: MutableList<SuccessorBundle<SlidingTilePuzzle4State>> = arrayListOf()
 
@@ -49,10 +60,23 @@ class SlidingTilePuzzle(val size: Int, val actionDuration: Long) : Domain<Slidin
         return null
     }
 
+    /**
+     * Returns the state's heuristic value multiplied by action duration.
+     *
+     * @param state is the state to provide a heuristic for
+     * @return the heuristic value multiplied by action duration
+     */
     override fun heuristic(state: SlidingTilePuzzle4State): Double {
         return state.heuristic * actionDuration
     }
 
+    /**
+     * Returns a heuristic for a sliding tile puzzle state: the sum of Manhattan distance of each tile from its goal position. This function calculates a heuristic value from one given state to another given state, rather than the standard sliding tile puzzle goal.
+     *
+     * @param startState is the current state
+     * @param endState is the goal state
+     * @return the sum of Manhattan distances for all numbered tiles
+     */
     override fun heuristic(startState: SlidingTilePuzzle4State, endState: SlidingTilePuzzle4State): Double {
         var manhattanSum = 0.0
         var zero: Byte = 0
@@ -78,6 +102,12 @@ class SlidingTilePuzzle(val size: Int, val actionDuration: Long) : Domain<Slidin
         return manhattanSum
     }
 
+    /**
+     * Returns a heuristic for a sliding tile puzzle state: the sum of Manhattan distance of each tile from its goal position. This function calculates a heuristic value from the given state to the standard sliding tile goal state.
+     *
+     * @param state is the state to provide a heuristic for
+     * @return the sum of Manhattan distances for all numbered tiles
+     */
     fun initialHeuristic(state: SlidingTilePuzzle4State): Double {
         var manhattanSum = 0.0
         var zero: Byte = 0
@@ -94,8 +124,20 @@ class SlidingTilePuzzle(val size: Int, val actionDuration: Long) : Domain<Slidin
         return manhattanSum
     }
 
+    /**
+     * Returns the distance from a given state to the goal. Always the same as heuristic value.
+     *
+     * @param state is the state to provide a heuristic for
+     * @return distance to goal, always the same as heuristic
+     */
     override fun distance(state: SlidingTilePuzzle4State) = state.heuristic
 
+    /**
+     * Returns a boolean indicating whether the given state is a goal state.
+     *
+     * @param state is the state to provide a heuristic for
+     * @return true if state is a goal state, otherwise false
+     */
     override fun isGoal(state: SlidingTilePuzzle4State) = state.heuristic == 0.0
 
     override fun print(state: SlidingTilePuzzle4State): String {
